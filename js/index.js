029afc5fef2e881a5c0fa33332dfff3fb3b9080f
Josefine Kristiansson <josephinekrist@hotmail.com>
2022-11-10 16:55:53 +0100

    reset and counter

--- js/index.js
+++ js/index.js
@@ -2,37 +2,45 @@
 let inputForm, button, listNumbers, correct, toLow, toHigh, guessCount;
 // alla document komsi hit
 inputForm = document.getElementById("inputForm");
+let userGuess = document.getElementById("inputForm");
 let randomNumber = (document.getElementById("randomNumber").innerHTML =
-  Math.floor(Math.random() * 10) + 1);
+  Math.floor(Math.random() * 100) + 1);
 const btn = document.querySelector("button");
 guessCount = 0;
-guessCount = document.getElementById("guessCount").innerHTML =
-  "Guesses so far " + guessCount + "!";
 
-/* btn.addEventListener("click", play); */
+function playAgain() {
+  location.reload();
+}
 
-// function som ser ifall random nummert och input är equal
+function setGuessInformation(information, shouldTextBeBig) {
+  let guessInformationElement = document.getElementById("guessInformation");
+  guessInformationElement.style.display = "block";
+  guessInformationElement.innerHTML = information;
+  if (shouldTextBeBig) {
+    guessInformationElement.classList.add("bigText");
+  }
+}
 
 function play() {
-  inputForm.value;
-  guessCount += 1;
+  guessCount = document.getElementById("guesses").innerHTML = guessCount + 1;
+  document.getElementById("previousGuesses").innerHTML +=
+    " " + inputForm.value + " ";
   if (inputForm.value == randomNumber) {
-    document.getElementById("correct").innerHTML =
-      "You are correct! <br> The number was " + randomNumber + "!";
-    document.getElementById("toLow").style.display = "none";
-    document.getElementById("toHigh").style.display = "none";
+    document.getElementById("reset").style.display = "block";
+    setGuessInformation(
+      "You are correct! <br> The number was " + randomNumber + "!",
+      true
+    );
+    document.getElementById("inputForm").style.display = "none";
+    document.getElementById("myButton").style.display = "none";
     console.log("Woho");
   } else if (inputForm.value < randomNumber) {
-    document.getElementById("toLow").innerHTML =
-      "You are to low! Try a higher number!";
-    document.getElementById("toHigh").style.display = "none";
-    document.getElementById("toLow").style.display = "block";
+    setGuessInformation("You are to low! Try a higher number!", false);
+    document.getElementById("reset").style.display = "none";
     console.log("Buhu");
   } else if (inputForm.value > randomNumber) {
-    document.getElementById("toHigh").innerHTML =
-      "You are to high! Try a lower number!";
-    document.getElementById("toLow").style.display = "none";
-    document.getElementById("toHigh").style.display = "block";
+    setGuessInformation("You are to high! Try a lower number!", false);
+    document.getElementById("reset").style.display = "none";
     console.log("Buhu");
   }
 }
